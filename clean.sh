e6b9afb1804848c358ee6d0fa1e6bb77945c25d5
yacquub <97082343+bblankfield@users.noreply.github.com>
2024-03-07 19:52:46 -0500

    Fix empty folders

--- clean.sh
+++ clean.sh
@@ -5,6 +5,7 @@ TRANSLATIONS_PATH="./fanyi/translations/"
 
 # Clean the raws and translations directory
 clean_directory() {
+	# Find a way to ignore .gitkeep files
 	find "$1" -mindepth 1 -delete
 }
 
