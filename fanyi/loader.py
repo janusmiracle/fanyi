ab2ccd5e801bc1b9fb788731c3d39f50182d932f
yacquub <97082343+bblankfield@users.noreply.github.com>
2024-03-17 03:11:06 -0400

    Fix `load_data` docstring.

--- fanyi/loader.py
+++ fanyi/loader.py
@@ -13,8 +13,8 @@ def load_data(
     limit: Optional[int],
 ) -> Generator[Dict[str, str], None, None]:
     """
-    Lazy load raw and translated text files from directories and yield a dictionary
-    containing the filename and the raw and translated text of each file.
+    Lazy-load raw and translated text files, yielding dictionaries
+    containing the raw text and translated text of each file.
 
     Parameters
     ----------
@@ -37,14 +37,14 @@ def load_data(
         If source_directory does not exist.
     """
     if source_directory is None:
-        raise ValueError("source_directory cannot be None")
+        raise ValueError('source_directory cannot be None.')
 
     if not source_directory.exists():
-        raise FileNotFoundError(f"Directory {source_directory} does not exist.")
+        raise FileNotFoundError(f'Directory {source_directory} does not exist.')
 
     # Sort directories to ensure consistent ordering after importing
-    raw_files = sort_files(source_directory.joinpath("raws"))
-    translated_files = sort_files(source_directory.joinpath("translations"))
+    raw_files = sort_files(source_directory.joinpath('raws'))
+    translated_files = sort_files(source_directory.joinpath('translations'))
 
     if limit:
         raw_files = itertools.islice(raw_files, limit)
@@ -56,14 +56,14 @@ def load_data(
         translated_text = translated_path.read_text()
 
         yield {
-            "raw_text": raw_text,
-            "translated_text": translated_text,
+            'raw_text': raw_text,
+            'translated_text': translated_text,
         }
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     for data in load_data(
-        import_data(Path(os.getcwd() + "/tests/custom_dataset/chinese/"), "chinese"),
+        import_data(Path(os.getcwd() + '/tests/custom_dataset/chinese/'), 'chinese'),
         limit=10,
     ):
         pass
