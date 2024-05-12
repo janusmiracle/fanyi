9c03908d1525eb7cc3a5501fa09fe37657b4b92c
yacquub <97082343+bblankfield@users.noreply.github.com>
2024-05-12 14:56:15 -0400

    Remove old tests

--- tests/run_tests.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from test_helsinki import TestHelsinkiModels
-from test_imports import TestImportRaws, TestImportTranslations
-from test_utils import TestUtils
-
-# Run each test
-if __name__ == '__main__':
-    unittest.main()
